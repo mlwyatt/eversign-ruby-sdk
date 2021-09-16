cca0914c9af4592511ccb8e053d7d3cc01d5541e
Marcus Wyatt <mlwyatt2008@gmail.com>
2021-09-16 12:01:25 -0400

    fix: initialize fields with attributes
    version bump

--- lib/eversign/models/field.rb
+++ lib/eversign/models/field.rb
@@ -1,8 +1,18 @@
 module Eversign
-	module Models
-		class Field
-      attr_accessor :name, :type, :x, :y, :width, :height, :page, :signer, :identifier, :required, :readonly, :merge, :type,
-		    					:validation_type, :text_style, :text_font, :text_size, :text_color, :value, :options, :group
-		end
-	end
+  module Models
+    class Field
+      ATTRIBUTES = [
+        :name, :type, :x, :y, :width, :height, :page, :signer, :identifier, :required, :readonly, :merge, :type,
+        :validation_type, :text_style, :text_font, :text_size, :text_color, :value, :options, :group
+      ]
+
+      attr_accessor *ATTRIBUTES
+
+      def initialize(attrs)
+        ATTRIBUTES.each do |attr|
+          send("#{attr}=", attrs.fetch(attr, nil))
+        end
+      end
+    end
+  end
 end
